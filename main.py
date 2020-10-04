937acbcd45ca4a13dd85e2ab472658416c81c0a7
ESKYoung <ESKYoung@users.noreply.github.com>
2020-10-04 21:40:00 +0100

    Replace pybadges dependency with calls to Shields.IO API
    Call Shields.IO API directly to create the visitor count - this lets
    you leverage the different styles/colours/logos/etc. available in the
    Shields.IO API.
    
    By default, the label will be 'visitors', and the colour will be
    #118bee, as defined in the DEFAULT_LABEL_TEXT and DEFAULT_MESSAGE_COLOR
    environmental variables.
    
    The full list of optional query parameters sent to the Shields.IO API
    is in the URL_PARAMETER_SHIELDS_QUERY environmental variable.

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 import datetime
 
 from flask import Flask, Response, request, render_template
-from pybadges import badge
 from hashlib import md5
 import requests
 from os import environ
 from dotenv import find_dotenv,load_dotenv
+from typing import Optional
 
 load_dotenv(find_dotenv())
+
+URL_PARAMETER_PAGE_ID = environ.get('URL_PARAMETER_PAGE_ID')
+URL_PARAMETER_LABEL = environ.get('URL_PARAMETER_LABEL')
+URL_PARAMETER_COLOR = environ.get('URL_PARAMETER_COLOR')
+URL_PARAMETER_SHIELDS_QUERY = environ.get('URL_PARAMETER_SHIELDS_QUERY').split(' ')
+DEFAULT_LABEL_TEXT = environ.get('DEFAULT_LABEL_TEXT')
+DEFAULT_MESSAGE_COLOR = environ.get('DEFAULT_MESSAGE_COLOR')
 
 app = Flask(__name__)
 
@@ -17,13 +24,18 @@ def invalid_count_resp(err_msg) -> Response:
     Return a svg badge with error info when cannot process repo_id param from request
     :return: A response with invalid request badge
     """
-    svg = badge(left_text="Error", right_text=err_msg,
-                whole_link="https://github.com/jwenjian/visitor-badge")
+    svg = requests.get('https://img.shields.io/badge/{label}-{message}-{color}?link={link}'.format(
+            label='Error',
+            message=err_msg,
+            color=DEFAULT_MESSAGE_COLOR,
+            link='https://github.com/jwenjian/visitor-badge'
+        )
+    )
     expiry_time = datetime.datetime.utcnow() - datetime.timedelta(minutes=10)
 
-    headers = {'Cache-Control': 'no-cache,max-age=0', 'Expires': expiry_time.strftime("%a, %d %b %Y %H:%M:%S GMT")}
+    headers = {'Cache-Control': 'no-cache,max-age=0', 'Expires': expiry_time.strftime('%a, %d %b %Y %H:%M:%S GMT')}
 
-    return Response(response=svg, content_type="image/svg+xml", headers=headers)
+    return Response(response=svg, content_type='image/svg+xml', headers=headers)
 
 
 def update_counter(key):
@@ -41,29 +53,41 @@ def update_counter(key):
 @app.route("/badge")
 def visitor_svg() -> Response:
     """
-    Return a svg badge with latest visitor count of 'Referer' header value
+    Return a svg badge with latest visitor count of 'Referrer' header value
 
     :return: A svg badge with latest visitor count
     """
 
-    req_source = identity_request_source()
+    req_source = get_request_value(URL_PARAMETER_PAGE_ID)
 
     if not req_source:
-        return invalid_count_resp('Missing required param: page_id')
+        return invalid_count_resp(f'Missing required param: {URL_PARAMETER_PAGE_ID}')
 
     latest_count = update_counter(req_source)
 
     if not latest_count:
         return invalid_count_resp("Count API Failed")
 
-    svg = badge(left_text="visitors", right_text=str(latest_count))
+    label_text = get_request_value(URL_PARAMETER_LABEL)
+    color_value = get_request_value(URL_PARAMETER_COLOR)
+
+    shields_query_strings = list(filter(None, [get_request_value(p) for p in URL_PARAMETER_SHIELDS_QUERY]))
+
+    shields_url = 'https://img.shields.io/badge/{label}-{message}-{color}{query}'.format(
+        label=label_text if label_text else DEFAULT_LABEL_TEXT,
+        message=str(latest_count),
+        color=color_value if color_value and len(color_value) else DEFAULT_MESSAGE_COLOR,
+        query='?{}'.format('&'.join(shields_query_strings)) if shields_query_strings else ''
+    )
+
+    svg = requests.get(shields_url)
 
     expiry_time = datetime.datetime.utcnow() - datetime.timedelta(minutes=10)
 
     headers = {'Cache-Control': 'no-cache,max-age=0,no-store,s-maxage=0,proxy-revalidate',
-               'Expires': expiry_time.strftime("%a, %d %b %Y %H:%M:%S GMT")}
+               'Expires': expiry_time.strftime('%a, %d %b %Y %H:%M:%S GMT')}
 
-    return Response(response=svg, content_type="image/svg+xml", headers=headers)
+    return Response(response=svg, content_type='image/svg+xml', headers=headers)
 
 
 @app.route("/index.html")
@@ -73,13 +97,19 @@ def index() -> Response:
     return render_template('index.html')
 
 
-def identity_request_source() -> str:
-    page_id = request.args.get('page_id')
-    if page_id is not None and len(page_id):
-        m = md5(page_id.encode('utf-8'))
-        m.update(environ.get('md5_key').encode('utf-8'))
-        return m.hexdigest()
-    return None
+def get_request_value(url_parameter_name: str) -> Optional[str]:
+    url_parameter_value = request.args.get(url_parameter_name)
+    if url_parameter_value is not None and len(url_parameter_value):
+        if url_parameter_name == URL_PARAMETER_PAGE_ID:
+            m = md5(url_parameter_value.encode('utf-8'))
+            m.update(environ.get('md5_key').encode('utf-8'))
+            return m.hexdigest()
+        elif url_parameter_name in URL_PARAMETER_SHIELDS_QUERY:
+            return f'{url_parameter_name}={url_parameter_value}'
+        else:
+            return url_parameter_value
+    else:
+        return None if url_parameter_value is None else url_parameter_value
 
 
 if __name__ == '__main__':
